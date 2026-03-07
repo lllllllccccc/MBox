3db6650d391ff3615957a5cc76574b119898c43c
lllllllccccc <3485440799@qq.com>
2026-03-07 17:06:00 +0800

    Update MusicBox.java

--- src/MusicBox.java
+++ src/MusicBox.java
@@ -19,10 +19,18 @@ public class MusicBox {
         this.songArtists = artists;
         
         // -- TODO
+MusicBox mbox2 = new MusicBox("A12BC34", 'N', "Sunshine", "Bright", 
+    "Happy day", "Pop", "Star Music", "Sunny Vibes");
+
+playsong.playSong(mbox2.getSongID(), mbox2.getPremiumSong(), 1);
         /* Initialise  the rest of the variables in this Constructor block*/
     }
     
-    
+    this.songTitle = title;       
+        this.songAlbum = Album;       
+        this.songGenre = genre;       
+        this.songProducer = producer;
+        this.songMusicLabel = label;  
     //Methods
     public String getSongID() {
         return songID;
@@ -37,8 +45,29 @@ public class MusicBox {
     }
 
 
-    // -- TODO
-        /* Create the rest of "getters" for the remaining variables */
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public String getSongAlbum() {
+        return songAlbum;
+    }
+
+    public String getSongGenre() {
+        return songGenre;
+    }
+
+    public String getSongProducer() {
+        return songProducer;
+    }
+
+    public String getSongMusicLabel() {
+        return songMusicLabel;
+    }
+
+    public int getNoAds() {
+        return noAds;
+    }
     
 }
 
