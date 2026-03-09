bdc3b8d8a1591e7ea6a63c9289efd3b80dd66e39
lllllllccccc <3485440799@qq.com>
2026-03-09 21:15:05 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,6 @@ public class Main{
         MusicBox mbox2 = new MusicBox("X789KL01", 'N', "Sunset", "Ocean Breeze", 
         "Morning Light", "Pop", "Blue Wave Band", "Coastal Vibes");
         
-        // 2. 调用 playSong 方法，ads 参数传 1
         playsong.playSong(mbox2.getSongID(), mbox2.getPremiumSong(), 1);
         
 
