dd8997e4e0ce07e884a4cc0735736d11c6262873
lllllllccccc <3485440799@qq.com>
2026-03-09 21:14:36 +0800

    Update ERyder.java

--- src/ERyder.java
+++ src/ERyder.java
@@ -1,12 +1,10 @@
-// ERyder 类（非public）
-class ERyder {
-    // 成员变量
+
+class ERyder{
     private int bikeID;
     private int batteryLevel;
     private boolean isAvailable;
     private double kmDriven;
-
-    // 默认构造函数
+    
     public ERyder() {
         this.bikeID = 0;
         this.batteryLevel = 0;
@@ -14,7 +12,7 @@ class ERyder {
         this.kmDriven = 0.0;
     }
 
-    // 带所有参数的构造函数
+   
     public ERyder(int bikeID, int batteryLevel, boolean isAvailable, double kmDriven) {
         this.bikeID = bikeID;
         this.setBatteryLevel(batteryLevel);
@@ -22,8 +20,8 @@ class ERyder {
         this.kmDriven = kmDriven;
     }
 
-    // ride() 方法
-    public void ride() {
+
+    public void ride(){
         if (this.batteryLevel > 0 && this.isAvailable) {
             System.out.println("自行车可用，可以骑行。");
         } else {
@@ -31,7 +29,6 @@ class ERyder {
         }
     }
 
-    // printBikeDetails() 方法
     public void printBikeDetails() {
         System.out.println("自行车ID: " + this.bikeID);
         System.out.println("电池电量: " + this.batteryLevel + "%");
@@ -40,7 +37,6 @@ class ERyder {
         System.out.println("------------------------");
     }
 
-    // Getter 和 Setter 方法
     public int getBikeID() {
         return bikeID;
     }
@@ -78,22 +74,17 @@ class ERyder {
     }
 }
 
-// Main 类（必须是public，且文件名必须为Main.java）
 public class Main {
     public static void main(String[] args) {
-        // 1. 使用默认构造函数创建对象，并调用printBikeDetails()
+       
         ERyder bike1 = new ERyder();
         System.out.println("自行车1的初始信息：");
         bike1.printBikeDetails();
-
-        // 2. 使用带参数的构造函数创建对象，调用ride()和printBikeDetails()
         ERyder bike2 = new ERyder(1001, 80, true, 150.5);
         System.out.println("尝试骑行车2：");
         bike2.ride();
         System.out.println("自行车2的详细信息：");
         bike2.printBikeDetails();
-
-        // 测试setBatteryLevel的范围检查
         System.out.println("尝试将自行车2的电量设置为150%：");
         bike2.setBatteryLevel(150);
         bike2.printBikeDetails();
