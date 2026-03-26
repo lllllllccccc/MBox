d8fcf40db9a7f0f5885da328ff5abed3e88328f2
lllllllccccc <3485440799@qq.com>
2026-03-26 21:31:08 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,12 @@
-
 public class Main{
     public static void main(String[] args) {
-        
-        MusicBox mbox = new MusicBox("S34TG65", 'Y', "Raindrops", "Misty", 
-        "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
 
-        PlaySongs playsong = new PlaySongs();
-        
-        playsong.playSong(mbox.getSongID(), mbox.getPremiumSong(), 2);
-
-        
-    
-        MusicBox mbox2 = new MusicBox("X789KL01", 'N', "Sunset", "Ocean Breeze", 
-        "Morning Light", "Pop", "Blue Wave Band", "Coastal Vibes");
-        
-        playsong.playSong(mbox2.getSongID(), mbox2.getPremiumSong(), 1);
+        UserRegistration user = new UserRegistration();
+        user.registration();
+        System.out.println(user);
+       AdminPanel adminPanel=new AdminPanel();
+         adminPanel.userManagementOptions();
         
 
     }
 }
-
-
-
-
